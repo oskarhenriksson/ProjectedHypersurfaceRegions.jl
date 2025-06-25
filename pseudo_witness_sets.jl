588a9f72b0907b3c857285bf154218f653e52c26
Oskar Henriksson <oskar.david.henriksson@gmail.com>
2025-06-25 23:30:20 +0200

    Fix bug in new PseudoWitnessSet
    The A matrix and b vector should have complex entries.

--- pseudo_witness_sets.jl
+++ pseudo_witness_sets.jl
@@ -18,8 +18,8 @@ Generates a pseudo witness set for the system `F` under the assumption that the 
 function PseudoWitnessSet(F::System, k::Int, codimension; L::Union{LinearSubspace, Nothing} = nothing) 
     n = nvariables(F)
     if isnothing(L)
-        A = hcat(rand(codimension, k), zeros(codimension, n-k))
-        b = rand(codimension)
+        A = hcat(rand(ComplexF64, codimension, k), zeros(codimension, n-k))
+        b = rand(ComplexF64, codimension)
         L = LinearSubspace(A, b)
     else
         @assert codim(L) == codimension "The codimension of the given linear subspace L must match the codimension of the pseudo witness set."
@@ -52,12 +52,12 @@ function track_pws_to_lines(
     PWS::PseudoWitnessSet,
 )
     L = PWS.L
-    new_directions = map(bj -> lifted_line(point, bj, ambient_dim(L)), eachcol(directions))
+    new_lines = map(bj -> lifted_line(point, bj, ambient_dim(L)), eachcol(directions))
     HC.solve(
         PWS.F,
         PWS.W,
         start_subspace = L,
-        target_subspaces = new_directions,
+        target_subspaces = new_lines,
         intrinsic = true,
         transform_result = (r, p) -> solutions(r),
     )
