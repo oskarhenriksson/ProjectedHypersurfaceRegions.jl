03609e5ff9485ccdeeccf8ee0a6ceb3a70d307fd
Oskar Henriksson <oskar.david.henriksson@gmail.com>
2025-06-27 14:46:28 +0200

    Fix bug in cluster example
    The definition of F_ordered had accidentally been removed

--- examples/Cluster_stab.jl
+++ examples/Cluster_stab.jl
@@ -2,11 +2,11 @@ using Pkg
 Pkg.activate("..")
 include("../src/functions.jl");
 
-@var κ[1:6] x[1:2] c[1:2]
+@var κ[1:6] x[1:2] T[1:2]
 # κ chemical reaction rate coefficients
-# c conservation constants
+# T conservation constants (total amounts)
 # x chemical concentrations
-Clus_Eq = [x[1]+x[2]-c[1]-c[2], 
+Clus_Eq = [x[1]+x[2]-T[1]-T[2], 
 -κ[1]*x[1]^3*x[2]-2*κ[2]*x[1]^3*x[2]-3*κ[3]*x[1]^3*x[2]-κ[4]*x[1]^3*x[2]^2-2*κ[5]*x[1]^2*x[2]^2-κ[6]x[1]*x[2]^3]
 # Equations of Cluster Stabilization Model 
 # Example 3.1 from https://www.sciencedirect.com/science/article/pii/S0196885821000920
@@ -15,8 +15,8 @@ Jac = differentiate(Clus_Eq, [x[1], x[2]])
 
 dJ = det(Jac)
 
-F = System([Clus_Eq; dJ], variables = [κ; c; x])
-projection_vars = [κ; c[1]; c[2]]  # These are the variables we are projecting to.
+F = System([Clus_Eq; dJ], variables = [κ; T; x])
+projection_vars = [κ; T]  # These are the variables we are projecting to.
 
 
 
@@ -25,9 +25,13 @@ B = qr(rand(k, k)).Q |> Matrix
 c = 10 .* randn(k)
 e = 6 # e should be even and larger than floor(degree(PWS)/2) + 1. 
 
+F_ordered = System(F.expressions, variables = [projection_vars; x])
+
 hess_off_diag = hess_log_r(F_ordered, e, projection_vars; method = :off_diag, c, B)
 hess_many_slices = hess_log_r(F_ordered, e, projection_vars; method = :many_slices, c, B)
 
 p = rand(k)
-hess_off_diag(p)
-hess_many_slices(p)
+H_od = hess_off_diag(p)
+H_ms = hess_many_slices(p)
+
+norm(H_od-H_ms)
