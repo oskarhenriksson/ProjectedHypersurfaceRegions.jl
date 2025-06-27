4aaf8cb5979eb668c4590f8265f49ec023d748c7
Oskar Henriksson <oskar.david.henriksson@gmail.com>
2025-06-27 14:16:25 +0200

    Rename rate constants
    Rate constants are now κ (kappa), and myk is renamed to k (to unify the notation with the other example files).

--- examples/Cluster_stab.jl
+++ examples/Cluster_stab.jl
@@ -4,12 +4,12 @@ using HomotopyContinuation, LinearAlgebra, DifferentialEquations
 
 include("../src/functions.jl");
 
-@var k[1:6] xy xz cy cz
-# k chemical reaction rate coefficients
+@var κ[1:6] xy xz cy cz
+# κ chemical reaction rate coefficients
 # c conservation constants
 # x chemical concentrations
 Clus_Eq = [xy+xz-cy-cz, 
--k[1]*xy^3*xz-2*k[2]*xy^3*xz-3*k[3]*xy^3*xz-k[4]*xy^3*xz^2-2*k[5]*xy^2*xz^2-k[6]xy*xz^3]
+-κ[1]*xy^3*xz-2*κ[2]*xy^3*xz-3*κ[3]*xy^3*xz-κ[4]*xy^3*xz^2-2*κ[5]*xy^2*xz^2-κ[6]xy*xz^3]
 # Equations of Cluster Stabilization Model 
 # Example 3.1 from https://www.sciencedirect.com/science/article/pii/S0196885821000920
 
@@ -17,17 +17,17 @@ Jac = differentiate(Clus_Eq, [xy, xz])
 
 dJ = det(Jac)
 
-F = System([Clus_Eq; dJ], variables = [k[:]; cy; cz; xy; xz])
-myk = 8
-B = qr(rand(myk, myk)).Q |> Matrix
-c = 10 .* randn(myk)
+F = System([Clus_Eq; dJ], variables = [κ; cy; cz; xy; xz])
+k = 8
+B = qr(rand(k, k)).Q |> Matrix
+c = 10 .* randn(k)
 e = 6 # e should be even and larger than floor(degree(PWS)/2) + 1. 
-projection_vars = [k[:]; cy; cz]  # These are the variables we are projecting to.
+projection_vars = [κ; cy; cz]  # These are the variables we are projecting to.
 
 
 hess_off_diag = hess_log_r(F_ordered, e, projection_vars; method = :off_diag, c, B)
 hess_many_slices = hess_log_r(F_ordered, e, projection_vars; method = :many_slices, c, B)
 
-p = rand(myk)
+p = rand(k)
 hess_off_diag(p)
 hess_many_slices(p)
--- examples/Wnt_network.jl
+++ examples/Wnt_network.jl
@@ -8,29 +8,29 @@ using HomotopyContinuation, LinearAlgebra, DifferentialEquations
 include("../src/functions.jl");
 
 
-@var k[1:31] x[1:19] c[1:5]
+@var κ[1:31] x[1:19] c[1:5]
 # Define the system of equations for the Wnt Signaling Pathway
 # Goal: We want to say something about the real discriminant of this system (so eliminate the x's)
 # k reaction rate coefficients
 # c conservation constants
 # x chemical concentrations
 chemical_eqns = [
-    #-k[1]*x[1]+k[2]*x[2], 
-    k[1]*x[1] - (k[2]+k[26])*x[2]+k[27]*x[3]-k[3]*x[2]*x[4]+(k[4]+k[5])*x[14],
-    k[26]*x[2]-k[27]*x[3]-k[14]*x[3]*x[6]+(k[15]+k[16])*x[15],
-    -k[3]*x[2]*x[4]-k[9]*x[4]*x[10]+k[4]*x[14]+k[8]*x[16]+(k[10]+k[11])*x[18],
-    -k[28]*x[5]+k[29]*x[7]-k[6]*x[5]*x[8]+k[5]*x[14]+k[7]*x[16],
-    -k[14]*x[3]*x[6]-k[20]*x[6]*x[11]+k[15]*x[15]+k[19]*x[17]+(k[21]+k[22])*x[19],
-    k[28]*x[5]-k[29]*x[7]-k[17]*x[7]*x[9]+k[16]*x[15]+k[18]*x[17],
-    -k[6]*x[5]*x[8]+(k[7]+k[8])*x[16], 
-    -k[17]*x[7]*x[9]+(k[18]+k[19])*x[17], 
-    k[12]-(k[13]+k[30])*x[10]-k[9]*x[4]*x[10]+k[31]*x[11]+k[10]*x[18],
-    -k[23]*x[11]+k[30]*x[10]-k[31]*x[11]-k[20]*x[6]*x[11]-k[24]*x[11]*x[12]+k[25]*x[13]+k[21]*x[19], 
-    -k[24]*x[11]*x[12]+k[25]*x[13],
-    k[3]*x[2]*x[4]-(k[4]+k[5])*x[14], 
-    k[14]*x[3]*x[6]-(k[15]+k[16])*x[15], 
-    k[9]*x[4]*x[10]-(k[10]+k[11])*x[18], 
-    #k[20]*x[6]*x[11]-(k[21]+k[22])*x[19], 
+    #-κ[1]*x[1]+κ[2]*x[2], 
+    κ[1]*x[1] - (κ[2]+κ[26])*x[2]+κ[27]*x[3]-κ[3]*x[2]*x[4]+(κ[4]+κ[5])*x[14],
+    κ[26]*x[2]-κ[27]*x[3]-κ[14]*x[3]*x[6]+(κ[15]+κ[16])*x[15],
+    -κ[3]*x[2]*x[4]-κ[9]*x[4]*x[10]+κ[4]*x[14]+κ[8]*x[16]+(κ[10]+κ[11])*x[18],
+    -κ[28]*x[5]+κ[29]*x[7]-κ[6]*x[5]*x[8]+κ[5]*x[14]+κ[7]*x[16],
+    -κ[14]*x[3]*x[6]-κ[20]*x[6]*x[11]+κ[15]*x[15]+κ[19]*x[17]+(κ[21]+κ[22])*x[19],
+    κ[28]*x[5]-κ[29]*x[7]-κ[17]*x[7]*x[9]+κ[16]*x[15]+κ[18]*x[17],
+    -κ[6]*x[5]*x[8]+(κ[7]+κ[8])*x[16], 
+    -κ[17]*x[7]*x[9]+(κ[18]+κ[19])*x[17], 
+    κ[12]-(κ[13]+κ[30])*x[10]-κ[9]*x[4]*x[10]+κ[31]*x[11]+κ[10]*x[18],
+    -κ[23]*x[11]+κ[30]*x[10]-κ[31]*x[11]-κ[20]*x[6]*x[11]-κ[24]*x[11]*x[12]+κ[25]*x[13]+κ[21]*x[19], 
+    -κ[24]*x[11]*x[12]+κ[25]*x[13],
+    κ[3]*x[2]*x[4]-(κ[4]+κ[5])*x[14], 
+    κ[14]*x[3]*x[6]-(κ[15]+κ[16])*x[15], 
+    κ[9]*x[4]*x[10]-(κ[10]+κ[11])*x[18], 
+    #κ[20]*x[6]*x[11]-(κ[21]+κ[22])*x[19], 
     x[1]+x[2]+x[3]+x[14]+x[15]-c[1], 
     x[4]+x[5]+x[6]+x[7]+x[14]+x[15]+x[16]+x[17]+x[18]+x[19]-c[2],
     x[8]+x[16]-c[3],
@@ -41,18 +41,18 @@ chemical_eqns = [
 #Jac = differentiate(chemical_eqns,x)
 #dJ = det(Jac) #This is taking forever!
 # So lets just try to eliminats the x's from the chemical_eqns (without the Jacobian).
-F = System(chemical_eqns, variables = [k; c; x])
+F = System(chemical_eqns, variables = [κ; c; x])
 # System for the incidence variety of the discriminant
-myk = 36
-B = qr(rand(myk,myk)).Q |> Matrix
-myc = 10 .* randn(myk)
+k = 36
+B = qr(rand(k,k)).Q |> Matrix
+myc = 10 .* randn(k)
 e = 20
 # e should be at least  floor(degree(PWS)/2) + 1
 all_vars = variables(F)
-projection_vars = [k;c]
+projection_vars = [κ;c]
 x_vars = setdiff(all_vars, projection_vars)
 F_ordered = System(F.expressions, variables = [projection_vars; x_vars])
-PWS = PseudoWitnessSet(F_ordered, myk; linear_subspace_codim = myk-1) # issue! Our linear space intersects at something larger than points.
+PWS = PseudoWitnessSet(F_ordered, k; linear_subspace_codim = k-1) # issue! Our linear space intersects at something larger than points.
 
 # u + tv this in R^(36). Lift it to R^(55). So codimension of (u+tv) was 35, and the pullback to R^(55) still has codimension 35. So it has dimension 20.
 L = create_line(u, v, size(F_ordered, 2))
