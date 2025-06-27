5dfaf6fdc38191ee880b17c4031a2c1174be7a47
Oskar Henriksson <oskar.david.henriksson@gmail.com>
2025-06-27 14:35:40 +0200

    Minor clean-ups of examples

--- examples/Cluster_stab.jl
+++ examples/Cluster_stab.jl
@@ -1,29 +1,29 @@
 using Pkg
-Pkg.activate(".")
-using HomotopyContinuation, LinearAlgebra, DifferentialEquations
-
+Pkg.activate("..")
 include("../src/functions.jl");
 
-@var κ[1:6] xy xz cy cz
+@var κ[1:6] x[1:2] c[1:2]
 # κ chemical reaction rate coefficients
 # c conservation constants
 # x chemical concentrations
-Clus_Eq = [xy+xz-cy-cz, 
--κ[1]*xy^3*xz-2*κ[2]*xy^3*xz-3*κ[3]*xy^3*xz-κ[4]*xy^3*xz^2-2*κ[5]*xy^2*xz^2-κ[6]xy*xz^3]
+Clus_Eq = [x[1]+x[2]-c[1]-c[2], 
+-κ[1]*x[1]^3*x[2]-2*κ[2]*x[1]^3*x[2]-3*κ[3]*x[1]^3*x[2]-κ[4]*x[1]^3*x[2]^2-2*κ[5]*x[1]^2*x[2]^2-κ[6]x[1]*x[2]^3]
 # Equations of Cluster Stabilization Model 
 # Example 3.1 from https://www.sciencedirect.com/science/article/pii/S0196885821000920
 
-Jac = differentiate(Clus_Eq, [xy, xz])
+Jac = differentiate(Clus_Eq, [x[1], x[2]])
 
 dJ = det(Jac)
 
-F = System([Clus_Eq; dJ], variables = [κ; cy; cz; xy; xz])
+F = System([Clus_Eq; dJ], variables = [κ; c; x])
+projection_vars = [κ; c[1]; c[2]]  # These are the variables we are projecting to.
+
+
+
 k = 8
 B = qr(rand(k, k)).Q |> Matrix
 c = 10 .* randn(k)
 e = 6 # e should be even and larger than floor(degree(PWS)/2) + 1. 
-projection_vars = [κ; cy; cz]  # These are the variables we are projecting to.
-
 
 hess_off_diag = hess_log_r(F_ordered, e, projection_vars; method = :off_diag, c, B)
 hess_many_slices = hess_log_r(F_ordered, e, projection_vars; method = :many_slices, c, B)
--- examples/Wnt_network.jl
+++ examples/Wnt_network.jl
@@ -1,14 +1,18 @@
-# This is our attempt to compute the Wint chemical reaction network discriminant.
+# This is our attempt to compute the Wnt chemical reaction network discriminant.
 # Cant continue till we find the determinant of the jacobian, which is a 19x19 matrix. )
 
-using Pkg
-Pkg.activate(".")
-using HomotopyContinuation, LinearAlgebra, DifferentialEquations
 
+### Idea for avoiding computing the determinant of the Jacobian:
+# Encode that the Jacobian is singular by adding equations Jac*v=0 and rand(19)⋅v-1=0
+# In addition to avoiding the slow and memory intensive determinant, I think this also performs better numerically
+# since it replaces a dense polynomial of high degree with 19 spare polynomials of low degree
+
+using Pkg
+Pkg.activate("..")
 include("../src/functions.jl");
 
 
-@var κ[1:31] x[1:19] c[1:5]
+@var κ[1:31] x[1:19] T[1:5]
 # Define the system of equations for the Wnt Signaling Pathway
 # Goal: We want to say something about the real discriminant of this system (so eliminate the x's)
 # k reaction rate coefficients
@@ -31,27 +35,30 @@ chemical_eqns = [
     κ[14]*x[3]*x[6]-(κ[15]+κ[16])*x[15], 
     κ[9]*x[4]*x[10]-(κ[10]+κ[11])*x[18], 
     #κ[20]*x[6]*x[11]-(κ[21]+κ[22])*x[19], 
-    x[1]+x[2]+x[3]+x[14]+x[15]-c[1], 
-    x[4]+x[5]+x[6]+x[7]+x[14]+x[15]+x[16]+x[17]+x[18]+x[19]-c[2],
-    x[8]+x[16]-c[3],
-    x[9]+x[17]-c[4], 
-    x[12]+x[13]-c[5]]
+    x[1]+x[2]+x[3]+x[14]+x[15]-T[1], 
+    x[4]+x[5]+x[6]+x[7]+x[14]+x[15]+x[16]+x[17]+x[18]+x[19]-T[2],
+    x[8]+x[16]-T[3],
+    x[9]+x[17]-T[4], 
+    x[12]+x[13]-T[5]]
 #The commented out equations are redundant.
 
 #Jac = differentiate(chemical_eqns,x)
 #dJ = det(Jac) #This is taking forever!
 # So lets just try to eliminats the x's from the chemical_eqns (without the Jacobian).
-F = System(chemical_eqns, variables = [κ; c; x])
-# System for the incidence variety of the discriminant
-k = 36
-B = qr(rand(k,k)).Q |> Matrix
-myc = 10 .* randn(k)
-e = 20
-# e should be at least  floor(degree(PWS)/2) + 1
+F = System(chemical_eqns, variables = [κ; T; x])
 all_vars = variables(F)
-projection_vars = [κ;c]
+projection_vars = [κ; T]
 x_vars = setdiff(all_vars, projection_vars)
 F_ordered = System(F.expressions, variables = [projection_vars; x_vars])
+
+# System for the incidence variety of the discriminant
+k = length(projection_vars)
+B = qr(rand(k,k)).Q |> Matrix
+c = 10 .* randn(k)
+e = 20
+# e should be at least  floor(degree(PWS)/2) + 1
+
+
 PWS = PseudoWitnessSet(F_ordered, k; linear_subspace_codim = k-1) # issue! Our linear space intersects at something larger than points.
 
 # u + tv this in R^(36). Lift it to R^(55). So codimension of (u+tv) was 35, and the pullback to R^(55) still has codimension 35. So it has dimension 20.
