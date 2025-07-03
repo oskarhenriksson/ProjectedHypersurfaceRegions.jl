f68df59cd348615c8c0cfe61a06d03d6c6727b0a
PBrdng <pbrdng@gmail.com>
2025-07-03 13:30:52 +0200

    reduce number of allocations for computing the gradient

--- src/pseudo_witness_sets.jl
+++ src/pseudo_witness_sets.jl
@@ -7,7 +7,7 @@ struct PseudoWitnessSet
     W::Result
 end
 degree(PWS::PseudoWitnessSet) = length(PWS.W)
-
+ambient_dim(PWS::PseudoWitnessSet) = HC.ambient_dim(PWS.L)
 
 @doc raw"""
     PseudoWitnessSet(F::System, k::Int; linear_subspace_codim::Int, L::LinearSubspace)
@@ -22,24 +22,29 @@ Optional inputs:
 - `L`: The linear space used for the witness set. Should be the preimage under $\pi$ of a linear subspace in $\mathbb{C}^k$
 
 """
-function PseudoWitnessSet(F::System, k::Int;
+function PseudoWitnessSet(
+    F::System,
+    k::Int;
     linear_subspace_codim::Union{Int,Nothing} = nothing,
-    L::Union{LinearSubspace,Nothing} = nothing
+    L::Union{LinearSubspace,Nothing} = nothing,
 )
     n = nvariables(F)
     if isnothing(linear_subspace_codim)
         linear_subspace_codim = n - length(F)
     end
     if isnothing(L)
-        A = hcat(rand(ComplexF64, linear_subspace_codim, k), zeros(linear_subspace_codim, n - k))
+        A = hcat(
+            rand(ComplexF64, linear_subspace_codim, k),
+            zeros(linear_subspace_codim, n - k),
+        )
         b = rand(ComplexF64, linear_subspace_codim)
         L = LinearSubspace(A, b)
     else
         @assert ambient_dim(L) == n "The ambient dimension of the linear subspace L must match the number of variables in the system F."
     end
-    startL = rand_subspace(n; codim=linear_subspace_codim)
+    startL = rand_subspace(n; codim = linear_subspace_codim)
     S = solutions(witness_set(F, startL))
-    W = HC.solve(F, S, start_subspace=startL, target_subspace=L, intrinsic=true)
+    W = HC.solve(F, S, start_subspace = startL, target_subspace = L, intrinsic = true)
     PseudoWitnessSet(F, k, L, W)
 end
 
@@ -49,7 +54,11 @@ end
 
 Lifts the line `point+t*direction` in $\mathbb{C}^k$ to a LinearSubspace in $\mathbb{C}^k\timess \mathbb{C}^{n-k}$.
 """
-function lifted_line(point::AbstractVector{<:Number}, direction::AbstractVector{<:Number}, n::Int64)
+function lifted_line(
+    point::AbstractVector{<:Number},
+    direction::AbstractVector{<:Number},
+    n::Int64,
+)
     k = length(point)
     πA = nullspace(direction')'
     A = hcat(πA, zeros(k - 1, n - k))
@@ -58,19 +67,32 @@ function lifted_line(point::AbstractVector{<:Number}, direction::AbstractVector{
 end
 
 
-function track_pws_to_lines(
+function track_pws_to_lines!(
+    GC::GradientCache,
     point::AbstractVector{<:Real},
     directions::AbstractArray{Float64},
     PWS::PseudoWitnessSet,
 )
-    L = PWS.L
-    new_lines = map(bj -> lifted_line(point, bj, ambient_dim(L)), eachcol(directions))
-    HC.solve(
-        PWS.F,
-        PWS.W,
-        start_subspace=L,
-        target_subspaces=new_lines,
-        intrinsic=true,
-        transform_result=(r, p) -> solutions(r),
-    )
+    n = ambient_dim(PWS)
+    for (j, bj) in enumerate(eachcol(directions))
+        GC.Ks[j] = lifted_line(point, bj, n)
+    end
+
+    # L = PWS.L
+    # new_lines = map(bj -> lifted_line(point, bj, ambient_dim(L)), eachcol(directions))
+    # HC.solve(
+    #     PWS.F,
+    #     PWS.W,
+    #     start_subspace=L,
+    #     target_subspaces=new_lines,
+    #     intrinsic=true,
+    #     transform_result=(r, p) -> solutions(r),
+    # )
+    for (j, K) in enumerate(GC.Ks)
+        target_parameters!(GC.tracker, K)
+        for (l, w) in enumerate(PWS.W)
+            track!(GC.tracker, solution(w), 1)
+            GC.line_hypersurface_intersections[j][l] .= solution(GC.tracker)
+        end
+    end
 end
