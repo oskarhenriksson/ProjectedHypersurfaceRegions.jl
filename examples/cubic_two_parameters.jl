da092b778e8a3c5441bd289aff019b20aa004a71
Oskar Henriksson <oskar.henriksson@mpi-cbg.de>
2025-10-14 16:51:24 +0200

    update cubic example
    Incorporated new plotting ideas from the quadratic example.

--- examples/cubic_two_parameters.jl
+++ examples/cubic_two_parameters.jl
@@ -1,7 +1,7 @@
 using Pkg
 Pkg.activate(".")
 
-using ImplicitPlots, Plots, DifferentialEquations, Random
+using ImplicitPlots, Plots, Random
 
 include("../src/functions.jl");
 
@@ -17,22 +17,29 @@ F = System([x^3 + a * x^2 + b*x + 1; 3*x^2 + 2*a*x + b], variables = [a, b, x])
 projection_variables = [a; b]
 k = length(projection_variables)
 
-B = qr(rand(k, k)).Q |> Matrix # not needed anymore (but kept for reproducibility)
-c = 10 .* randn(k)
+c = [13.758979284873828, -0.09884333335596635]
 r = RoutingGradient(F, projection_variables; c = c)
 
-# critical points
-res = critical_points(r)
-pts = real_solutions(res)
+# Critical points
+options = MonodromyOptions(
+    parameter_sampler = p -> 10 .* randn(ComplexF64, length(p)), # bigger loops
+    max_loops_no_progress = 10 # change the stopping criterion
+)
 
-##### Plotting 
+res0 = critical_points(r, options = options)
+pts = real_solutions(res0)
+
+# Connecting 
+G, idx, failed_info = partition_of_critical_points(r, pts)
+G
+
+# Plotting 
 M_x = maximum(p -> abs(p[1]), pts) + 6
 M_y = maximum(p -> abs(p[2]), pts) + 6
 
-
 # Discriminant
 h(x, y) = 4*x^3 - x^2*y^2 - 18*x*y + 4*y^3 + 27
-
+e = degree(r.PWS)
 R(x, y) = log(abs(h(x,y) / (1 + (x-c[1])^2 + (y-c[2])^2)^e))
 contour(
     (-M_x):0.1:M_x,
@@ -56,40 +63,82 @@ implicit_plot!(
     label = "discriminant"
 )
 
-# Gradient flow for routing points of positive index
+
+## plot flow
+pts1 = pts[idx .!= 0]
 g(x, param, t) = real(evaluate(r, x))
 tspan = (0.0, 1e4)
-for (i, u0) in enumerate(pts)
-    println()
-    println("Critical point #$i")
-    jac = real(evaluate_and_jacobian(r, u0)[2])
-    eigen_data = eigen(jac)
-    eigenvalues = eigen_data.values
-    eigenvectors = eigen_data.vectors
-    positive_directions = [i for (i, λ) in enumerate(eigenvalues) if real(λ) > 0]
-    println("Index: $(length(positive_directions))")
-    if !isempty(positive_directions)
-        idx = first(positive_directions)
-        v = eigenvectors[:, idx]
-        prob = ODEProblem(g, u0 + 0.01*v, tspan)
-        sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
-        plot!(Tuple.(sol.u), linecolor = :steelblue, linewidth = 4, label="")
-        prob = ODEProblem(g, u0 - 0.01*v, tspan)
-        sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
-        plot!(Tuple.(sol.u), linecolor = :steelblue, linewidth = 4, label="")
-        scatter!(Tuple(u0), markercolor = :magenta, markersize = 8, label = "")
-    else
-        scatter!(Tuple(u0), markercolor = :green, markersize = 8, label = "")
-    end
+for u0 in pts1
+	jac = real(evaluate_and_jacobian(r, u0)[2])
+	eigen_data = LinearAlgebra.eigen(jac)
+	eigenvalues = eigen_data.values
+	eigenvectors = eigen_data.vectors
+	positive_directions = [i for (i, λ) in enumerate(eigenvalues) if real(λ) > 0]
+	j = first(positive_directions)
+	v = eigenvectors[:, j]
 
+	prob = ODEProblem(g, u0 + 0.01*v, tspan)
+	sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
+	flow = Tuple.(sol.u)
+	l = length(flow)
+	k = div(l, 3)
+	plot!(flow[1:k], linecolor = :steelblue, linewidth = 3, label = false, arrow = true)
+	plot!(flow[k:end], linecolor = :steelblue, linewidth = 3, label = false)
+	prob = ODEProblem(g, u0 - 0.01*v, tspan)
+	sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
+	flow = Tuple.(sol.u)
+	l = length(flow)
+	k = div(l, 3)
+	plot!(flow[1:k], linecolor = :steelblue, linewidth = 3, label = false, arrow = true)
+	plot!(flow[k:end], linecolor = :steelblue, linewidth = 3, label = false)
 end
 
-# Legend
-plot!([], [], color = :steelblue, linewidth = 4, label = "gradient flow")
-scatter!(Tuple(NaN), markercolor = :green, markersize = 8, label = "routing pts (index 0)")
-scatter!(Tuple(NaN), markercolor = :magenta, markersize = 8, label = "routing pts (index > 0)")
+## plot critical points
+palette = collect(range(colorant"darkgreen", stop=colorant"lightgreen", length=length(G)))
+for (i, component) in enumerate(G)
+    scatter!(Tuple.(pts[component]), markercolor = palette[i], markersize = 8, label = "Critical points in region $i")
+end
 
 plot!(; legend = :bottomright, dpi=400, legendfontsize=6)
 
 savefig("./figures/example_cubic.svg")
 savefig("./figures/example_cubic.png")
+
+
+# # Gradient flow for routing points of positive index
+# g(x, param, t) = real(evaluate(r, x))
+# tspan = (0.0, 1e4)
+# for (i, u0) in enumerate(pts)
+#     println()
+#     println("Critical point #$i")
+#     jac = real(evaluate_and_jacobian(r, u0)[2])
+#     eigen_data = eigen(jac)
+#     eigenvalues = eigen_data.values
+#     eigenvectors = eigen_data.vectors
+#     positive_directions = [i for (i, λ) in enumerate(eigenvalues) if real(λ) > 0]
+#     println("Index: $(length(positive_directions))")
+#     if !isempty(positive_directions)
+#         idx = first(positive_directions)
+#         v = eigenvectors[:, idx]
+#         prob = ODEProblem(g, u0 + 0.01*v, tspan)
+#         sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
+#         plot!(Tuple.(sol.u), linecolor = :steelblue, linewidth = 4, label="")
+#         prob = ODEProblem(g, u0 - 0.01*v, tspan)
+#         sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
+#         plot!(Tuple.(sol.u), linecolor = :steelblue, linewidth = 4, label="")
+#         scatter!(Tuple(u0), markercolor = :magenta, markersize = 8, label = "")
+#     else
+#         scatter!(Tuple(u0), markercolor = :green, markersize = 8, label = "")
+#     end
+
+# end
+
+# # Legend
+# plot!([], [], color = :steelblue, linewidth = 4, label = "gradient flow")
+# scatter!(Tuple(NaN), markercolor = :green, markersize = 8, label = "routing pts (index 0)")
+# scatter!(Tuple(NaN), markercolor = :magenta, markersize = 8, label = "routing pts (index > 0)")
+
+# plot!(; legend = :bottomright, dpi=400, legendfontsize=6)
+
+# savefig("./figures/example_cubic.svg")
+# savefig("./figures/example_cubic.png")
