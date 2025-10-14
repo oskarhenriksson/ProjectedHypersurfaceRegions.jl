2f721b38dca40dd3237fc0efe6e68e726264d4b4
PBrdng <pbrdng@gmail.com>
2025-10-14 15:00:42 +0200

    add partition function

--- examples/quadratic.jl
+++ examples/quadratic.jl
@@ -1,4 +1,4 @@
-using Pkg, Random, Plots, DifferentialEquations, Random, Plots, DifferentialEquations, SciMLBase
+using Pkg, Random, Plots, DifferentialEquations, Random, Plots, DifferentialEquations, LightGraphs
 Pkg.activate(".")
 
 include("../src/functions.jl");
@@ -9,7 +9,7 @@ include("../src/functions.jl");
 F = System([x^2 + a * x + b; 2x + a], variables = [a, b, x])
 
 B = qr(rand(2, 2)).Q |> Matrix # not needed anymore (but kept for reproducibility)
-c = 10*randn(2)
+c = [0.15058793143477558; 11.593556720643692]
 r = RoutingGradient(F, [a, b]; c = c)
 
 p1 = zeros(2)
@@ -25,13 +25,13 @@ t0 = 1.0
 evaluate_and_jacobian!(u, U, H, x0, t0)
 
 
-### Test monodromy
+### monodromy
 res0 = critical_points(r)
 pts = real_solutions(res0)
 
-
 ### connecting 
-G = partition_of_critical_points(r, pts)
+G, idx, failed_info = partition_of_critical_points(r, pts)
+G
 
 
 
@@ -63,40 +63,47 @@ plot!(
     label = "discriminant",
 )
 
-# Gradient flow for routing points of positive index
-g(x, param, t) = real(evaluate(r, x))
-tspan = (0.0, 1e4)
-for (i, u0) in enumerate(pts)
-    println()
-    println(i)
-    jac = real(evaluate_and_jacobian(r, u0)[2])
-    eigen_data = eigen(jac)
-    eigenvalues = eigen_data.values
-    eigenvectors = eigen_data.vectors
-    positive_directions = [i for (i, λ) in enumerate(eigenvalues) if real(λ) > 0]
-    println("Index: $(length(positive_directions))")
-    if !isempty(positive_directions)
-        idx = first(positive_directions)
-        v = eigenvectors[:, idx]
-        prob = ODEProblem(g, u0 + 0.01*v, tspan)
-        sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
-        plot!(Tuple.(sol.u), linecolor = :steelblue, linewidth = 4, label="")
-        prob = ODEProblem(g, u0 - 0.01*v, tspan)
-        sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
-        plot!(Tuple.(sol.u), linecolor = :steelblue, linewidth = 4, label="")
-        scatter!(Tuple(u0), markercolor = :magenta, markersize = 8, label = "")
-    else
-        scatter!(Tuple(u0), markercolor = :green, markersize = 8, label = "")
-    end
+scatter!(Tuple.(pts[G[1]]), markercolor = :green, markersize = 8, label = "region 1")
+scatter!(Tuple.(pts[G[2]]), markercolor = :steelblue, markersize = 8, label = "region 2")
 
-end
 
-# Legend
-plot!([], [], color = :steelblue, linewidth = 4, label = "gradient flow")
-scatter!(Tuple(NaN), markercolor = :green, markersize = 8, label = "routing pts (index 0)")
-scatter!(Tuple(NaN), markercolor = :magenta, markersize = 8, label = "routing pts (index > 0)")
 
-plot!(; legend = :bottomright, dpi=400, legendfontsize=6)
 
-savefig("./figures/example_quadratic.png")
-savefig("./figures/example_quadratic.svg")
+
+# # Gradient flow for routing points of positive index
+# g(x, param, t) = real(evaluate(r, x))
+# tspan = (0.0, 1e4)
+# for (i, u0) in enumerate(pts)
+#     println()
+#     println(i)
+#     jac = real(evaluate_and_jacobian(r, u0)[2])
+#     eigen_data = eigen(jac)
+#     eigenvalues = eigen_data.values
+#     eigenvectors = eigen_data.vectors
+#     positive_directions = [i for (i, λ) in enumerate(eigenvalues) if real(λ) > 0]
+#     println("Index: $(length(positive_directions))")
+#     if !isempty(positive_directions)
+#         idx = first(positive_directions)
+#         v = eigenvectors[:, idx]
+#         prob = ODEProblem(g, u0 + 0.01*v, tspan)
+#         sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
+#         plot!(Tuple.(sol.u), linecolor = :steelblue, linewidth = 4, label="")
+#         prob = ODEProblem(g, u0 - 0.01*v, tspan)
+#         sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
+#         plot!(Tuple.(sol.u), linecolor = :steelblue, linewidth = 4, label="")
+#         scatter!(Tuple(u0), markercolor = :magenta, markersize = 8, label = "")
+#     else
+#         scatter!(Tuple(u0), markercolor = :green, markersize = 8, label = "")
+#     end
+
+# end
+
+# # Legend
+# plot!([], [], color = :steelblue, linewidth = 4, label = "gradient flow")
+# scatter!(Tuple(NaN), markercolor = :green, markersize = 8, label = "routing pts (index 0)")
+# scatter!(Tuple(NaN), markercolor = :magenta, markersize = 8, label = "routing pts (index > 0)")
+
+# plot!(; legend = :bottomright, dpi=400, legendfontsize=6)
+
+# savefig("./figures/example_quadratic.png")
+# savefig("./figures/example_quadratic.svg")
