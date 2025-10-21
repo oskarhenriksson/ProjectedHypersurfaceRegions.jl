abcc479cb1fe43ed3043e4fc5163d044bd6009a4
Oskar Henriksson <oskar.henriksson@mpi-cbg.de>
2025-10-21 21:49:07 +0200

    Clean up small examples
    Removed old out-commented plotting code.

--- examples/quadratic.jl
+++ examples/quadratic.jl
@@ -5,18 +5,20 @@ include("../src/functions.jl");
 
 Random.seed!(0x8b868320)
 
-########
+# Set up the system
 @var a b x
 F = System([x^2 + a * x + b; 2x + a], variables = [a, b, x])
 
+# Pick a center for the routing function
 c = [13.758979284873828, -0.09884333335596635]
+
+# Set up the routing function gradient
 r = RoutingGradient(F, [a, b]; c = c)
 
+# Test forming the routing point homotopies
 p1 = zeros(2)
 q1 = randn(2)
 H = RoutingPointsHomotopy(r, p1, q1)
-
-### Test evaluation
 u = randn(ComplexF64, 2)
 U = randn(ComplexF64, 2, 2)
 x0 = randn(ComplexF64, 2)
@@ -24,29 +26,28 @@ t0 = 1.0
 @time evaluate_and_jacobian!(u, U, H, x0, t0)
 evaluate_and_jacobian!(u, U, H, x0, t0)
 
-
-### monodromy
+# Options for the monodromy step
 options = MonodromyOptions(
     parameter_sampler = p -> 10 .* randn(ComplexF64, length(p)), # bigger loops
     max_loops_no_progress = 10 # change the stopping criterion
 )
 
-
+# Find the complex critical points 
 res0 = critical_points(r, options = options)
+
+# Restrict to the real critical points
 pts = real_solutions(res0)
 
-### connecting 
+# Connect the crticial points
 G, idx, failed_info = partition_of_critical_points(r, pts)
 G
-
-
 
 ##### Plotting 
 M_x = maximum(p -> abs(p[1]), pts) + 4
 M_y = maximum(p -> abs(p[2]), pts) + 3
 
-
-RR(x, y) = log(abs((x^2 - 4 * y) / (1 + (x-c[1])^2 + (y-c[2])^2)^2)) #This is our routing function
+# Countour plot of the routing function
+RR(x, y) = log(abs((x^2 - 4 * y) / (1 + (x-c[1])^2 + (y-c[2])^2)^2)) 
 contour(
 	(-M_x):0.1:M_x,
 	(-M_y):0.1:M_y,
@@ -59,7 +60,8 @@ contour(
 	fill = true,
 )
 
-A = [[b; b^2 / 4] for b in (-M_x):M_x] #discriminant of the quadratic #discriminant of the quadratic
+# Plot the discriminant
+A = [[b; b^2 / 4] for b in (-M_x):M_x]
 plot!(
 	Tuple.(A),
 	xlims = (-M_x, M_x),
@@ -69,8 +71,7 @@ plot!(
 	label = "discriminant",
 )
 
-
-## plot flow
+# Plot flows from the critical points
 pts1 = pts[idx .!= 0]
 g(x, param, t) = real(evaluate(r, x))
 tspan = (0.0, 1e4)
@@ -99,51 +100,13 @@ for u0 in pts1
 	plot!(flow[k:end], linecolor = :steelblue, linewidth = 3, label = false)
 end
 
-## plot critical points
+# Plot the critical points (colored by component)
 palette = collect(range(colorant"green", stop=colorant"lightgreen", length=length(G)))
 for (i, component) in enumerate(G)
     scatter!(Tuple.(pts[component]), markercolor = palette[i], markersize = 8, label = "Critical points in region $i")
 end
 
 plot!(; legend = :bottomright, dpi=400, legendfontsize=6)
-
-savefig("./figures/example_quadratic.png")
-savefig("./figures/example_quadratic.svg")
-
-# # Gradient flow for routing points of positive index
-# g(x, param, t) = real(evaluate(r, x))
-# tspan = (0.0, 1e4)
-# for (i, u0) in enumerate(pts)
-#     println()
-#     println(i)
-#     jac = real(evaluate_and_jacobian(r, u0)[2])
-#     eigen_data = eigen(jac)
-#     eigenvalues = eigen_data.values
-#     eigenvectors = eigen_data.vectors
-#     positive_directions = [i for (i, λ) in enumerate(eigenvalues) if real(λ) > 0]
-#     println("Index: $(length(positive_directions))")
-#     if !isempty(positive_directions)
-#         idx = first(positive_directions)
-#         v = eigenvectors[:, idx]
-#         prob = ODEProblem(g, u0 + 0.01*v, tspan)
-#         sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
-#         plot!(Tuple.(sol.u), linecolor = :steelblue, linewidth = 4, label="")
-#         prob = ODEProblem(g, u0 - 0.01*v, tspan)
-#         sol = DE.solve(prob, reltol = 1e-6, abstol = 1e-6)
-#         plot!(Tuple.(sol.u), linecolor = :steelblue, linewidth = 4, label="")
-#         scatter!(Tuple(u0), markercolor = :magenta, markersize = 8, label = "")
-#     else
-#         scatter!(Tuple(u0), markercolor = :green, markersize = 8, label = "")
-#     end
-
-# end
-
-# # Legend
-# plot!([], [], color = :steelblue, linewidth = 4, label = "gradient flow")
-# scatter!(Tuple(NaN), markercolor = :green, markersize = 8, label = "routing pts (index 0)")
-# scatter!(Tuple(NaN), markercolor = :magenta, markersize = 8, label = "routing pts (index > 0)")
-
-# plot!(; legend = :bottomright, dpi=400, legendfontsize=6)
 
 # savefig("./figures/example_quadratic.png")
 # savefig("./figures/example_quadratic.svg")
