5e44f6b824c0ebf0ddf864929048e6e203f38ee7
Oskar Henriksson <oskar.henriksson@mpi-cbg.de>
2025-10-24 19:14:36 +0200

    Adapt new examples to new `critical_points`

--- examples/spinodal.jl
+++ examples/spinodal.jl
@@ -34,65 +34,9 @@ r = RoutingGradient(F, ε)
 # This gives 6 but I know the discriminant has degree 72
 d = degree(r.PWS) 
 
-# Critical points
-
-### Homotopy
-k = size(r, 2) # number of variables
-p1 = zeros(k)
-q1 = randn(k)
-H = RoutingPointsHomotopy(r, p1, q1)
-
-### Use monodromy to solve ∇r = p0 for generic RHS
-egtracker = EndgameTracker(H) # we want to add options later 
-trackers = [egtracker]
-x₀ = zeros(ComplexF64, size(H, k))
-unique_points = UniquePoints(
-    x₀,
-    1;
-)
-trace = zeros(ComplexF64, length(x₀) + 1, 3)
-P = Vector{ComplexF64}
-options = MonodromyOptions(
-    parameter_sampler = p -> 10 .* randn(ComplexF64, length(p)), # bigger loops
-    max_loops_no_progress = 10 # change the stopping criterion
-)
-
-
-MS = HomotopyContinuation.MonodromySolver(
-    trackers,
-    HomotopyContinuation.MonodromyLoop{P}[],
-    unique_points,
-    ReentrantLock(),
-    options,
-    HomotopyContinuation.MonodromyStatistics(),
-    trace,
-    ReentrantLock(),
-)
-
-### start pair
-s0 = randn(ComplexF64, k)
-p0 = evaluate(r, s0)
-
-### monodromy 
-seed = rand(UInt32)
-mon_result = monodromy_solve(
-    MS,
-    s0,
-    p0,
-    seed
-)
-
-### move to parameter = 0
-intermediate_p = randn(ComplexF64, length(p0))
-start_parameters!(H, p0)
-target_parameters!(H, intermediate_p)
-result_intermediate = HomotopyContinuation.solve(H, solutions(mon_result))
-start_parameters!(H, intermediate_p)
-target_parameters!(H, zeros(ComplexF64, length(p0)))
-result = HomotopyContinuation.solve(H, result_intermediate)
-
-# Extract the real solutions
-pts = real_solutions(result)
+# Routing points
+res, mon_res = critical_points(r)
+pts = real_solutions(res)
 
 # Connected components
 G, idx, failed_info = partition_of_critical_points(r, pts)
