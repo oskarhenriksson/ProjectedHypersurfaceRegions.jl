2f7e290dcfefccd46f8d070e10ea51c78039a9c9
Oskar Henriksson <oskar.henriksson@mpi-cbg.de>
2025-11-04 20:59:14 +0100

    Use gradient flow approach for quadratic example

--- examples/quadratic.jl
+++ examples/quadratic.jl
@@ -33,7 +33,7 @@ options = MonodromyOptions(
 )
 
 # Find the complex critical points 
-pts, res0, mon_res = critical_points(r, num_gradient_flow_starts=0, verbose=true, options = options)
+pts, res0, mon_res = critical_points(r, options = options)
 
 # Try another round of monodromy (only if you think the first attempt missed solutions)
 #pts, res0, mon_res = critical_points(r, solutions(mon_res), parameters(mon_res), options = options)
