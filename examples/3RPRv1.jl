01fa9d1b5f50d6805ee55760b1830dfeb8d62e78
Oskar Henriksson <oskar.henriksson@mpi-cbg.de>
2025-12-07 23:07:30 +0100

    Add code for the 3RPRv1 example

---
+++ examples/3RPRv1.jl
@@ -0,0 +1,95 @@
+# This is case 0 of Example 5.2 in the overleaf file
+# Reference: https://arxiv.org/pdf/1903.06126
+
+using Random
+include("../src/functions.jl");
+mkpath("./results/3RPRv1")
+
+Random.seed!(12345)
+
+time_start_round1 = time()
+
+# Set up incidence variety of the discriminant
+a2 = 14 // 10;
+a3 = 7 // 10;
+b3 = 1;
+A2 = 16 // 10;
+A3 = 9 // 10;
+B3 = 6 // 10;
+
+@var p[1:2] φ[1:2] c[1:3]
+
+f = [φ[1]^2 + φ[2]^2 - 1,
+    p[1]^2 + p[2]^2 - 2 * (a3 * p[1] + b3 * p[2]) * φ[1] + 2 * (b3 * p[1] - a3 * p[2]) * φ[2] + a3^2 + b3^2 - c[1],
+    p[1]^2 + p[2]^2 - 2 * A2 * p[1] + 2 * ((a2 - a3) * p[1] - b3 * p[2] + A2 * a3 - A2 * a2) * φ[1] + 2 * (b3 * p[1] + (a2 - a3) * p[2] - A2 * b3) * φ[2]
+    + (a2 - a3)^2 + b3^2 + A2^2 - c[2],
+    p[1]^2 + p[2]^2 - 2 * (A3 * p[1] + B3 * p[2]) + A3^2 + B3^2 - c[3]
+]
+
+Jac = differentiate(f, [p; φ])
+F = System([f; det(Jac)], variables=[p; φ; c])
+projection_variables = c;
+
+# Set up routing function
+center = 5 * rand(length(projection_variables))
+write_parameters("./results/3RPRv1/center.txt", center)
+∇r = RoutingGradient(F, projection_variables; c=center);
+
+# Degree of the discriminant
+d = degree(∇r.PWS)
+println("Degree of discriminant: $d")
+
+# Routing points
+pts, res, mon_res = critical_points(∇r)
+
+# Connected components 
+G, idx, failed_info = partition_of_critical_points(∇r, pts)
+
+time_end_round1 = time()
+println("Computation time for round 1: $(time_end_round1 - time_start_round1) seconds")
+
+include("./analysis.jl");
+function analyze_and_save_result()
+    write_parameters("./results/3RPRv1/monodromy_parameters.txt", parameters(mon_res))
+    write_solutions("./results/3RPRv1/monodromy_result.txt", solutions(mon_res))
+    write_solutions("./results/3RPRv1/result.txt", solutions(res))
+    write_solutions("./results/3RPRv1/routing_points.txt", pts)
+    write("./results/3RPRv1/connected_components.txt", string(G))
+
+    println("Connected components: $(G)")
+    println("Indicies: $(idx)")
+    println("Failed info: $(failed_info)")
+    println()
+
+    analyze_result(∇r, pts, G, idx;
+        root_counting_system=System(f, variables=vcat(p, φ), parameters=projection_variables)
+    )
+end
+
+analyze_and_save_result()
+
+# Try another round of monodromy (only if you think the first attempt missed solutions)
+println("Running second round of monodromy...")
+time_start_round2 = time()
+old_number_of_monodromy_solutions = length(solutions(mon_res))
+
+options = MonodromyOptions(
+    parameter_sampler=p -> 100 .* randn(ComplexF64, length(p)), # larger loops
+    max_loops_no_progress=10 # stopping criterion
+)
+pts, res, mon_res = critical_points(∇r, solutions(mon_res), parameters(mon_res), options=options)
+
+G, idx, failed_info = partition_of_critical_points(∇r, pts)
+
+time_end_round2 = time()
+println("Additional computation time for round 2: $(time_end_round2 - time_start_round2) seconds")
+println("Total computation time for round 1 and 2: $((time_end_round1 - time_start_round1) + (time_end_round2 - time_start_round2)) seconds")
+
+
+if length(solutions(mon_res)) > old_number_of_monodromy_solutions
+    println("Found new solutions with additional monodromy round!")
+    analyze_and_save_result()
+else
+    println("No new solutions found in the additional monodromy round.")
+end
+
---
+++ examples/symbolic_discriminants/3RPRv1_ideal.jl
@@ -0,0 +1,24 @@
+using Oscar
+
+# Set up incidence variety of the discriminant
+a2 = 14//10; a3 = 7//10; b3 = 1; A2 = 16//10; A3 = 9//10; B3 = 6//10;
+R, φ, p, c = polynomial_ring(QQ, "φ"=>1:2, "p"=>1:2, "c"=>1:3)
+
+f = [ φ[1]^2 + φ[2]^2 - 1,
+      p[1]^2 + p[2]^2 - 2*(a3*p[1] +b3*p[2])*φ[1] + 2*(b3*p[1] - a3*p[2])*φ[2] + a3^2 + b3^2 - c[1],
+      p[1]^2 + p[2]^2 - 2*A2*p[1] + 2*((a2-a3)*p[1] - b3*p[2] + A2*a3 -A2*a2)*φ[1] + 2*(b3*p[1] + (a2-a3)*p[2] - A2*b3)*φ[2]
+        + (a2-a3)^2 + b3^2 + A2^2 -c[2],
+      p[1]^2 + p[2]^2 -2*(A3*p[1] + B3*p[2]) + A3^2 + B3^2 - c[3]
+    ]
+
+Jac = derivative.(f, transpose(vcat(p,φ)))
+detJac = det(matrix(R, Jac))
+
+I = ideal(R, [f; detJac])
+
+# Compute the discriminant
+Ielim = eliminate(I, vcat(φ, p))
+h = gens(Ielim)[1]
+
+total_degree(h) # 12
+length(terms(h)) # 455
