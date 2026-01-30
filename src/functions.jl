447285867507fe20eb993d9174d5e63384f37fc2
Oskar Henriksson <oskar.henriksson@mpi-cbg.de>
2026-01-30 02:32:40 +0100

    Import HC functions

--- src/functions.jl
+++ src/functions.jl
@@ -3,11 +3,18 @@ using HomotopyContinuation, LinearAlgebra, DifferentialEquations, LightGraphs, P
 const HC = HomotopyContinuation
 const DE = DifferentialEquations
 
+import HomotopyContinuation.evaluate!
+import HomotopyContinuation.evaluate_and_jacobian!
+import HomotopyContinuation.evaluate
+import HomotopyContinuation.taylor!
+import HomotopyContinuation.ModelKit.evaluate
+import HomotopyContinuation.ModelKit.nvariables
+import HomotopyContinuation.ModelKit.variables
 
 include("pseudo_witness_sets.jl")
 include("gradient_cache.jl")
+include("hypersurfaces.jl")
 include("routing_functions.jl")
 include("homotopy.jl")
 include("ode_solving.jl")
 include("graph.jl")
-include("hypersurfaces.jl")
