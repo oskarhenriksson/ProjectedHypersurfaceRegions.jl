039c91db65fb6b3e6ed3394230c58d3730b7dcb3
David Johnson <davidklassenjohnson@gmail.com>
2026-02-06 13:38:52 -0500

    changes to make code into the package ProjectedHypersurfaceRegions

---
+++ src/ProjectedHypersurfaceRegions.jl
@@ -0,0 +1,25 @@
+module ProjectedHypersurfaceRegions
+using HomotopyContinuation, LinearAlgebra, DifferentialEquations, LightGraphs, ProgressMeter
+
+const HC = HomotopyContinuation
+const DE = DifferentialEquations
+
+import HomotopyContinuation.evaluate!
+import HomotopyContinuation.evaluate_and_jacobian!
+import HomotopyContinuation.evaluate
+import HomotopyContinuation.taylor!
+import HomotopyContinuation.ModelKit.evaluate
+import HomotopyContinuation.ModelKit.nvariables
+import HomotopyContinuation.ModelKit.variables
+
+include("pseudo_witness_sets.jl")
+include("gradient_cache.jl")
+include("hypersurfaces.jl")
+include("routing_functions.jl")
+include("homotopy.jl")
+include("ode_solving.jl")
+include("graph.jl")
+
+export ProjectedHypersurface, RoutingFunction, RoutingGradient, critical_points, partition_of_critical_points, analyze_result
+
+end
