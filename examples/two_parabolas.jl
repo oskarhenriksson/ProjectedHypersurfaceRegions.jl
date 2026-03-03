09adc604e52e37b68ecc8b0c90e77a3c4f97b6e9
Oskar Henriksson <oskar.henriksson@mpi-cbg.de>
2026-03-03 20:09:35 +0100

    Add example of multiple hypersurfaces

---
+++ examples/two_parabolas.jl
@@ -0,0 +1,59 @@
+
+using Random, ProjectedHypersurfaceRegions
+mkpath("./results/two_parabolas");
+
+Random.seed!(1234)
+
+########
+
+# Incidence variety of discriminants
+@var a b x
+F1 = System([x^2 + a * x + b; 2*x + a], variables=[a, b, x])
+h1 = ProjectedHypersurface(F1, [a; b])
+
+@var z
+F2 = System([z^2 + a * z - b; 2*z + a], variables=[a, z, b])
+h2 = ProjectedHypersurface(F2, [a; b])
+
+# Routing gradient
+c = [7, 3]
+r = RoutingFunction([h1, h2]; c=c)
+
+# Critical points
+pts, res, mon_res = critical_points(r)
+
+write_parameters("./results/two_parabolas/monodromy_parameters.txt", parameters(mon_res))
+write_solutions("./results/two_parabolas/monodromy_result.txt", solutions(mon_res))
+write_solutions("./results/two_parabolas/result.txt", solutions(res))
+write_solutions("./results/two_parabolas/routing_points.txt", pts)
+
+# Connecting 
+G, idx, failed_info = partition_of_critical_points(r, pts)
+println("Connected components: $(G)")
+println("Indicies: $(idx)")
+println("Failed info: $(failed_info)")
+println()
+
+write("./results/two_parabolas/connected_components.txt", string(G))
+
+# Analyzw result
+include("./analysis.jl");
+M_x = maximum(p -> abs(p[1]), pts) + 6
+M_y = maximum(p -> abs(p[2]), pts) + 6
+analyze_result(r, pts, G, idx;
+    h = (x,y) -> (4*y-x^2)*(-4*y-x^2),
+    markersize=7,
+    arrowstyle=:simple,
+    flow_linewidth=3,
+    discriminant_linewidth=4,
+    legend=:bottomright,
+    contour_stepsize=0.1,
+    M_x_max=M_x,
+    M_x_min=-M_x,
+    M_y_max=M_y,
+    M_y_min=-M_y,
+)
+
+savefig("./figures/two_parabolas.pdf")
+savefig("./figures/two_parabolas.svg")
+savefig("./figures/two_parabolas.png")
